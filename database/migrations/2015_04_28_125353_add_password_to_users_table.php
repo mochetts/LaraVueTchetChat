be149e6c8a5ec8cd39e7910c5fe7eed3df1529c0
Martin Mochetti <m.mochetti@design-it.de>
2015-04-28 09:55:37 -0300

    adding password migration

---
+++ database/migrations/2015_04_28_125353_add_password_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPasswordToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->text('password');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('password');
+		});
+	}
+
+}
